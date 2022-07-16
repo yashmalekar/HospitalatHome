873101148e9d07241bff1c59966952854f847d6b
yash_malekar <heramb.malekar@gmail.com>
2022-07-16 16:20:16 +0530

    config updated

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 const app_config = {
-	api_url: 'https://home-hospital-backend.herokuapp.com/',
-    //api_url: 'http://localhost:5000',
+	api_url: 'https://homeathospital.herokuapp.com',
     projectTitle: 'Home Hospital',
     equipmentCategories: [
         'Health Monitoring',
