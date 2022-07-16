cdf26a2efc559996e8d322bec8d7b3715b8621a9
yash_malekar <heramb.malekar@gmail.com>
2022-07-16 15:46:30 +0530

    first commit

---
+++ src/config.js
@@ -0,0 +1,13 @@
+const app_config = {
+	api_url: 'https://home-hospital-backend.herokuapp.com/',
+    //api_url: 'http://localhost:5000',
+    projectTitle: 'Home Hospital',
+    equipmentCategories: [
+        'Health Monitoring',
+        'Life Support Equipment',
+        'Diagnostic Equipment',
+        'Durable Medical Equipment (DME)'   
+    ]
+}
+
+export default app_config;
